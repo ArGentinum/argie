5cbaf601a95a7e5fae104a2d8cbc3aad73f8afd5
Gowtham <banggowd123@gmail.com>
2024-09-15 12:27:27 +0530

    new commit

---
+++ Untitled-2.py
@@ -0,0 +1,11 @@
+d = "python"
+print(d)
+a = "python"
+b = 2.334
+c = 566
+if a == "python":
+    print(a)
+if isinstance (b,float) and b == 2.334:
+    print("%f"%b)
+if isinstance (c,int) and c == 566 :
+    print("%d"%c)
