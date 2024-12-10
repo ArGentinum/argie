3b23ebcbe51956707076d92638240b792a585680
ArGentinum <123313917+ArGentinum@users.noreply.github.com>
2024-12-10 15:59:11 +0530

    Update chatbot.c

--- chatbot.c
+++ chatbot.c
@@ -53,14 +53,16 @@ int main(){
 //program to find compound interest
 int main(){
     float p,r,t;
+int n;
     printf("enter the principal: ");
     scanf("%f",&p);
     printf("enter the time(in years): ");
     scanf("%f",&t);
     printf("enter the rate: ");
     scanf("%f",&r);
-    float si=p* (pow((1+r/100),t)) - p;
-    printf("compound interest is :%f",si);
+printf("enter the frequency of compunding interest in a year:");
+scanf("%d",&n);
+printf("amount returned after %d  years is %f",t,p*pow(1+r/(100*n),n*t);
     return 0;
 }
 
